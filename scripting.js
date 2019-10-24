c336c5eab04aaea60c09cd5d4fca2789ad848d1d
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-10-24 12:42:36 +0530

    Add files via upload

---
+++ scripting.js
@@ -0,0 +1,14 @@
+function toggle()
+{
+	document.getElementById("content").classList.toggle('after');
+}
+
+function info()
+{
+	document.getElementById("ins").classList.add('after');
+}
+
+function home()
+{
+	document.getElementById("ins").classList.remove('after');
+}
