783d36f32a721493ad25a8170edc7b0e201d5072
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-10-25 17:49:55 +0530

    Add files via upload

--- scripting.js
+++ scripting.js
@@ -12,3 +12,38 @@ function home()
 {
 	document.getElementById("ins").classList.remove('after');
 }
+
+
+function save()
+{
+    	
+        const text = document.getElementById("textarea");
+        let data = text.value;
+        if (data!= "")
+        // Convert the text to BLOB.
+        {const textToBLOB = new Blob([data], { type: 'text/plain' });
+        const sFileName = 'Yourtext.txt';	   // The file to save the data.
+
+        let newLink = document.createElement("a");
+        newLink.download = sFileName;
+
+        // if (window.webkitURL != null) {
+        //     newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+        // }
+        // else {
+        //     newLink.href = window.URL.createObjectURL(textToBLOB);
+        //     newLink.style.display = "none";
+        //     document.body.appendChild(newLink);
+        // }
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+        newLink.click();}
+}
+
+function clearing()
+{
+	var t = prompt("Do you want to clear the text?(Y/N)");
+	if (t == "Y" || t == "y")
+	{
+		document.getElementById("textarea").value="";
+	}
+}
