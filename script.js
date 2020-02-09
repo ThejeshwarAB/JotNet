fc52c83e15ade849d1cc8f2ca6adae73e8d4d553
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2020-02-09 23:49:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function sending() {
     var subject = prompt("Enter subject:");
 
     console.log(message);
-    $.post("https://jotnet.herokuapp.com/process_post",
+    $.post("https://jotnet.herokuapp.com/",
         {
             text: message,
             mail: mail,
